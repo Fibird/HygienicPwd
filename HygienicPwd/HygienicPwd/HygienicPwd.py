b93fc5791802e688a70fe474e271e17d3f02661e
Chaoyang Liu <chaoyanglius@gmail.com>
2017-01-25 21:09:58 +0800

    design the apparence

--- HygienicPwd/HygienicPwd/HygienicPwd.py
+++ HygienicPwd/HygienicPwd/HygienicPwd.py
@@ -6,6 +6,12 @@ class MyWindow(Window):
     def __init__(self):
         wpf.LoadComponent(self, 'HygienicPwd.xaml')
     
+    def generate_Click(self, sender, e):
+        pass
+class Pwd:
+    def Save():
+        pass
+
 
 if __name__ == '__main__':
     Application().Run(MyWindow())
